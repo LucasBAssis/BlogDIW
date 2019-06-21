13dd433e1c2828285a419127e8442b13ec56bce7
Lucas Assis <lucas.andrade322@gmail.com>
2019-06-21 15:56:10 -0300

    Projeto Blog

---
+++ js/fakedb.js
@@ -0,0 +1,133 @@
+var dbfake = {
+    "data": [
+        {
+            "id": 1,
+            "titulo": "Lorem Ipsum",
+            "texto": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus at ex quam. Nulla sollicitudin eu lorem et ultricies. Sed lectus tellus, viverra vitae imperdiet eu, pharetra pulvinar magna. Sed ut mollis dolor. Sed leo tortor, laoreet quis aliquam nec, faucibus vel urna. Curabitur finibus odio id mi ornare tincidunt. Donec et tempus libero. Suspendisse molestie non tellus a vehicula. Curabitur sit amet ligula arcu. Donec ornare gravida sem a semper. Etiam viverra facilisis enim et porta. Etiam laoreet metus vitae ex scelerisque, ac consectetur velit venenatis. Morbi mollis nulla dui, tincidunt condimentum nunc mattis ut. Phasellus faucibus diam vitae massa venenatis vulputate. Sed fringilla massa id elit rhoncus rhoncus. Phasellus laoreet luctus gravida. Cras laoreet ex augue, et egestas erat tempus sed. Etiam sed vulputate arcu. Sed imperdiet auctor nisi, id sodales augue tristique rhoncus. Sed id aliquam elit. Sed laoreet libero sit amet libero interdum cursus eget et nunc. Phasellus convallis mattis tempus. Suspendisse sed dapibus ante, vitae auctor nibh. Nullam ac maximus metus.",
+            "data": new Date().toLocaleDateString(),
+            "imagem": "../img/nome_aqui",
+            "comentarios": [
+                {
+                    "nome": "Usuario 1",
+                    "comentario": "Bla bla bla"
+                }
+            ],
+            "curtidas": 0
+        },
+		{
+            "id": 2,
+            "titulo": "Brasil x Argentina? Veja possibilidades para clássico nas quartas da Copa América",
+            "texto": "O empate da Argentina por 1 a 1 com o Paraguai, nesta quarta-feira, em Belo Horizonte, levantou imediatamente a possibilidade de um duelo com o Brasil logo nas quartas de final da Copa América. Para isso ocorrer, porém, a combinação não é tão simples assim... São dois casos possíveis: No primeiro, o Brasil, necessariamente, precisa ficar em primeiro lugar no Grupo A: para isso, basta vencer o Peru, no próximo sábado, às 16h (de Brasília), na Arena Corinthians. Empate também serve, desde que a Venezuela não vença a Bolívia por quatro ou mais gols de diferença; nesse caso, a Vinotinto seria líder da chave.",
+            "data": new Date().toLocaleDateString(),
+            "imagem": "../img/nome_aqui",
+            "comentarios": [
+                {
+                    "nome": "Usuario 1",
+                    "comentario": "Bla bla bla"
+                }
+            ],
+            "curtidas": 0
+        },
+		{
+            "id": 3,
+            "titulo": "Estrela do rock e ex-militar: quem é Keanu Reeves em Cyberpunk 2077?",
+            "texto": "Keanu Reeves foi, sem dúvida, a personalidade que chamou a atenção na E3 2019. O ator carismático, que interpretou personagens icônicos em filmes como Matrix e John Wick, apareceu de surpresa na apresentação da Microsoft para revelar que seria um personagem no aguardado jogo Cyberpunk 2077, que chega em 16 de abril de 2020. E não será um personagem qualquer. Primeiro, porém, a má notícia: você não poderá controlar Keanu Reeves no jogo. Pelo menos o estúdio CD Projekt Red já adiantou que o personagem dele será o segundo com mais falas no game, atrás apenas do protagonista, chamado V.",
+            "data": new Date().toLocaleDateString(),
+            "imagem": "../img/nome_aqui",
+            "comentarios": [
+                {
+                    "nome": "Usuario 1",
+                    "comentario": "Bla bla bla"
+                }
+            ],
+            "curtidas": 0
+        },
+		{
+            "id": 4,
+            "titulo": "O que é Lorem Ipsum?",
+            "texto": "Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado. Se popularizou na década de 60, quando a Letraset lançou decalques contendo passagens de Lorem Ipsum, e mais recentemente quando passou a ser integrado a softwares de editoração eletrônica como Aldus PageMaker. Porque nós o usamos? É um fato conhecido de todos que um leitor se distrairá com o conteúdo de texto legível de uma página quando estiver examinando sua diagramação. A vantagem de usar Lorem Ipsum é que ele tem uma distribuição normal de letras, ao contrário de Conteúdo aqui, conteúdo aqui, fazendo com que ele tenha uma aparência similar a de um texto legível. Muitos softwares de publicação e editores de páginas na internet agora usam Lorem Ipsum como texto-modelo padrão, e uma rápida busca por 'lorem ipsum' mostra vários websites ainda em sua fase de construção. Várias versões novas surgiram ao longo dos anos, eventualmente por acidente, e às vezes de propósito (injetando humor, e coisas do gênero).",
+            "data": new Date().toLocaleDateString(),
+            "imagem": "../img/nome_aqui",
+            "comentarios": [
+                {
+                    "nome": "Usuario 1",
+                    "comentario": "Bla bla bla"
+                }
+            ],
+            "curtidas": 0
+        },
+		{
+            "id": 5,
+            "titulo": "Toy Story 4 | Crítica",
+            "texto": "“Você tem de prometer que vai cuidar bem desses caras. Eles significam muito pra mim”, diz Andy ao entregar seus brinquedos para a pequena Bonnie, pouco antes de se despedir e dirigir um último olhar a Woody e companhia, naquela já antológica cena na varanda, em Toy Story 3 (2010). Além de todo simbolismo presente — o término da infância, a inexorabilidade do tempo, o amor compartilhado por meio dos jogos e brincadeiras transmitidos entre gerações —, outro fator que torna a sequência uma das mais tocantes na história da animação é o fato de que, naquele momento, somos levados a nos identificar com o humano. O adeus de Andy é também o adeus do espectador aos personagens que acompanhou por 15 anos, e o próprio reconhecimento de que a jornada se encerra ali é o que leva à contemplação de seu propósito e à sensação de completude. Não é surpresa, portanto, que muita gente tenha torcido o nariz para a ideia de uma continuação. Em meio a especulações a respeito dos rumos que a história poderia tomar após a irretocável conclusão, o anúncio de um novo capítulo da franquia foi recebido com mais ceticismo que empolgação. No entanto, com a chegada do filme aos cinemas, a boa notícia é que, no geral, ele não decepciona. Toy Story 4 diverte e comove. Nesse sentido, faz jus a seus antecessores.",
+            "data": new Date().toLocaleDateString(),
+            "imagem": "../img/nome_aqui",
+            "comentarios": [
+                {
+                    "nome": "Usuario 1",
+                    "comentario": "Bla bla bla"
+                }
+            ],
+            "curtidas": 0
+        }
+    ]
+}
+
+// Caso exista no Local Storage, recupera os dados salvos
+var dbLucasBorges = JSON.parse(localStorage.getItem('dbLucasBorges'));
+if (!dbLucasBorges) {
+    dbLucasBorges = dbfake;
+};
+
+// Exibe mensagem em um elemento de ID msg
+function displayMessage(msg) {
+    $('#msg').html('<div class="alert alert-warning">' + msg + '</div>');
+}
+
+function insertPost(post) {
+    let novoId = dbLucasBorges.data[dbLucasBorges.data.length - 1].id + 1;
+    let novoPost = {
+        "id": novoId,
+        "titulo": post.titulo,
+        "texto": post.texto,
+        "data": new Date().toLocaleDateString(),
+        "imagem": "../img/nome_imagem",
+        "comentarios": [],
+        "curtidas": 0
+    };
+
+    // Insere o novo objeto no array
+    dbLucasBorges.data.push(novoPost);
+    displayMessage("Contato inserido com sucesso");
+
+    // Atualiza os dados no Local Storage
+    localStorage.setItem('dbLucasBorges', JSON.stringify(dbLucasBorges));
+}
+
+function insertComentario(id, comentario) {
+    // Localiza o indice do objeto a ser alterado no array a partir do seu ID
+    let index = dbLucasBorges.data.map(obj => obj.id).indexOf(id);
+
+    let novoComentario = {
+        "nome": comentario.nome,
+        "comentario": comentario.comentario
+    };
+
+    // Insere o comentário no array de Comentários do Post
+    dbLucasBorges.data[index].comentarios.push(novoComentario);
+    displayMessage("Comentário inserido com sucesso");
+
+    // Atualiza os dados no Local Storage
+    localStorage.setItem('dbLucasBorges', JSON.stringify(dbLucasBorges));
+}
+
+function curtir(id) {
+    // Localiza o indice do objeto a ser alterado no array a partir do seu ID
+    let index = dbLucasBorges.data.map(obj => obj.id).indexOf(id);
+
+    // Altera os dados do objeto no array
+    dbLucasBorges.data[index].curtidas = dbLucasBorges.data[index].curtidas + 1;
+
+    // Atualiza os dados no Local Storage
+    localStorage.setItem('dbLucasBorges', JSON.stringify(dbLucasBorges));
+}
